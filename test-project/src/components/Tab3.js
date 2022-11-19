5382c1ba99239c26aec873f749569b53e609f07a
Nguyen Chinh <chinhhvpkkq12345@gmail.com>
2022-11-19 13:09:36 +0700

    commit-push

---
+++ test-project/src/components/Tab3.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Tab3 = () => {
+  return (
+    <div>
+      <h2>Tab3</h2>
+      <p>
+        Sau đó xe máy nam thanh niên đã xảy ra va chạm với xe máy 85B1... của
+        một đàn ông điều khiển sát lề đường. Va chạm làm hai xe máy té ngã, cả
+        hai đều bị thương rất nặng, nhập viện trong tình trạng nguy kịch. Đúng
+        lúc này, một chiến sĩ CSGT điều khiển xe đặc chủng lưu thông tới đã
+        không kịp xử lý và va chạm vào hiện trường dẫn tới té ngã. CSGT bị gãy
+        xương đòn, phải nhập viện cấp cứu. Nhận tin báo, Công an TP Thủ Đức đã
+        có mặt khám nghiệm hiện trường, trích xuất camera, lấy lời khai những
+        người liên quan để làm rõ. Sau đó xe máy nam thanh niên đã xảy ra va
+        chạm với xe máy 85B1... của một đàn ông điều khiển sát lề đường. Va chạm
+        làm hai xe máy té ngã, cả hai đều bị thương rất nặng, nhập viện trong
+        tình trạng nguy kịch. Đúng lúc này, một chiến sĩ CSGT điều khiển xe đặc
+        chủng lưu thông tới đã người liên quan để làm rõ. Sau đó xe máy nam
+        thanh niên đã xảy ra va chạm với xe máy 85B1... của một đàn ông điều
+        khiển sát lề đường. Va chạm làm hai xe máy té ngã, cả hai đều bị thương
+        rất nặng, nhập viện trong tình trạng nguy kịch. Đúng lúc này, một chiến
+        sĩ CSGT điều khiển xe đặc chủng lưu thông tới đã
+      </p>
+    </div>
+  );
+};
+
+export default Tab3;
