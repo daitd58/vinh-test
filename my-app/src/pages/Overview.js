4a7ceeeafedb38e20de17a24771562b36770b0d9
taii <tai.cumanhtuan@ncc.asia>
2022-11-19 18:34:39 +0700

    thang

---
+++ my-app/src/pages/Overview.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Body from '../components/Body'
+import Header from '../components/Header'
+
+const Overview = () => {
+  return (
+    <div>
+      
+      <Body></Body>
+    </div>
+  )
+}
+
+export default Overview
