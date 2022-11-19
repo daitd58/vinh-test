7161be595dbd3ad608742b847e0b243d7bbf6369
SON <hongson.bth123@gmail.com>
2022-11-19 13:15:33 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './compumant/header'
+import Content from './compumant/content'
+import Content1 from './compumant/content1'
+
+function App() {
+  return (
+    <div>
+        <Header/>
+        <Content/>
+        <Content1/>
+    </div>
+
+  );
+}
+
+export default App;
