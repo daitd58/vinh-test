b56ea150a2952bc76767c0e1ed8b2c0545ae4c1e
phanvietvinh <114334225+phanvietvinh@users.noreply.github.com>
2022-11-19 13:23:52 +0700

    comit-code

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+import { Routes, Route } from 'react-router-dom';
+
+
+
+function App() {
+  return (
+    <div className='app'>
+      <nav>
+      <ul>
+        <li><a href="/Overview">Overview</a></li>
+        <li><a href="/Annoucement">Annoucement</a></li>
+        <li><a href="/Cousre">Cousre content</a></li>
+        <li><a href="/">Bookmark</a></li>
+        <li><a href="/">Q&A</a></li>
+        <li><a href="/">Grade</a></li>   
+    </ul>
+      </nav>
+    <hr/>
+    <Routes>
+      <Route path="/Overview" element={<Overview/>}></Route>
+      <Route path="/Annoucement" element={<Annoucement/>}></Route>
+      <Route path="/Cousre" element={<Cousre/>}></Route>
+    </Routes>
+    </div>
+            
+    
+    
+
+
+  )
+}
+
+export default App;
